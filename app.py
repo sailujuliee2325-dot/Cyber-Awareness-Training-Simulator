2ad8769c8bd7e9dd7355166191f6b18f53af7cb3
sailujuliee2325-dot <sailujuliee2325@gmail.com>
2026-01-05 18:06:15 +0530

    Added basic cyber awareness simulator code

---
+++ app.py
@@ -0,0 +1,16 @@
+print("Welcome to Cyber Awareness Training Simulator")
+
+print("1.Phishing Awareness")
+print("2.Password Safely")
+print("3.Safe Internet Practices")
+
+choice = input("Enter your choice:")
+
+if choice == "1":
+  print("Never click on unknown links or emails.")
+elif choice == "2":
+  print("Use strong passwords with symbols and numbers.")
+elif choice == "3":
+  print("Do not share personal information online.")
+else:
+  print("Invalid Choice")
